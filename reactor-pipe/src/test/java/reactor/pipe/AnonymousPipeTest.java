17c6603c488ccd0df0f1d0e5ec1586397380b04f
Alex Petrov <oleksandr.petrov@gmail.com>
2015-11-19 23:34:06 +0100

    Unify tests

--- reactor-pipe/src/test/java/reactor/pipe/AnonymousPipeTest.java
+++ reactor-pipe/src/test/java/reactor/pipe/AnonymousPipeTest.java
@@ -18,124 +18,32 @@ import java.util.concurrent.TimeUnit;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 
-public class AnonymousPipeTest extends AbstractFirehoseTest {
+public class AnonymousPipeTest extends AbstractPipeTest {
 
-  @Test
-  public void mapTest() throws InterruptedException {
-    AVar<String> res = new AVar<>();
-
-    Pipe.<Integer>build()
-      .map(i -> i + 1)
-      .map(i -> i * 2)
-      .map(Object::toString)
-      .consume(res::set)
-      .subscribe(Key.wrap("source", "first"),
-                 firehose);
-
-    firehose.notify(Key.wrap("source", "first"), 1);
-
-    assertThat(res.get(1, TimeUnit.SECONDS), is("4"));
+  @Override
+  protected <T, O> void subscribe(IPipe.PipeEnd<T, O> pipe) {
+    pipe.subscribe(Key.wrap("source", "first"),
+                   firehose);
   }
-//
-//  @Test
-//  public void statefulMapTest() throws InterruptedException {
-//    AVar<Integer> res = new AVar<>(3);
-//    NamedPipe<Integer> intPipe = new NamedPipe<>(firehose);
-//
-//    intPipe.anonymous(Key.wrap("key1"))
-//           .map((i) -> i + 1)
-//           .map((Atom<Integer> state, Integer i) -> {
-//                  return state.update(old -> old + i);
-//                },
-//                0)
-//           .consume(res::set);
-//
-//    intPipe.notify(Key.wrap("key1"), 1);
-//    intPipe.notify(Key.wrap("key1"), 2);
-//    intPipe.notify(Key.wrap("key1"), 3);
-//
-//    assertThat(res.get(LATCH_TIMEOUT, LATCH_TIME_UNIT), is(9));
-//  }
-//
-//  @Test
-//  public void testFilter() throws InterruptedException {
-//    NamedPipe<Integer> pipe = new NamedPipe<>(firehose);
-//    AVar<Integer> res = new AVar<>();
-//
-//    pipe.anonymous(Key.wrap("source"))
-//        .map(i -> i + 1)
-//        .filter(i -> i % 2 != 0)
-//        .map(i -> i * 2)
-//
-//        .consume(res::set);
-//
-//
-//    pipe.notify(Key.wrap("source"), 1);
-//    pipe.notify(Key.wrap("source"), 2);
-//
-//    assertThat(res.get(1, TimeUnit.SECONDS), is(6));
-//  }
-//
-//  @Test
-//  public void testPartition() throws InterruptedException {
-//    NamedPipe<Integer> pipe = new NamedPipe<>(firehose);
-//    AVar<List<Integer>> res = new AVar<>();
-//
-//    pipe.anonymous(Key.wrap("source"))
-//        .partition((i) -> {
-//          return i.size() == 5;
-//        })
-//        .consume(res::set);
-//
-//    pipe.notify(Key.wrap("source"), 1);
-//    pipe.notify(Key.wrap("source"), 2);
-//    pipe.notify(Key.wrap("source"), 3);
-//    pipe.notify(Key.wrap("source"), 4);
-//    pipe.notify(Key.wrap("source"), 5);
-//    pipe.notify(Key.wrap("source"), 6);
-//    pipe.notify(Key.wrap("source"), 7);
-//
-//    assertThat(res.get(1, TimeUnit.SECONDS), is(TreePVector.from(Arrays.asList(1, 2, 3, 4, 5))));
-//  }
-//
-//  @Test
-//  public void testSlide() throws InterruptedException {
-//    NamedPipe<Integer> pipe = new NamedPipe<>(firehose);
-//    AVar<List<Integer>> res = new AVar<>(6);
-//
-//    pipe.anonymous(Key.wrap("source"))
-//        .slide(i -> {
-//          return i.subList(i.size() > 5 ? i.size() - 5 : 0,
-//                           i.size());
-//        })
-//        .consume(res::set);
-//
-//    pipe.notify(Key.wrap("source"), 1);
-//    pipe.notify(Key.wrap("source"), 2);
-//    pipe.notify(Key.wrap("source"), 3);
-//    pipe.notify(Key.wrap("source"), 4);
-//    pipe.notify(Key.wrap("source"), 5);
-//    pipe.notify(Key.wrap("source"), 6);
-//
-//    assertThat(res.get(1, TimeUnit.SECONDS), is(TreePVector.from(Arrays.asList(2, 3, 4, 5, 6))));
-//  }
-//
-//  @Test
-//  public void testNotify() throws InterruptedException {
-//    NamedPipe<Integer> pipe = new NamedPipe<>(firehose);
-//    AVar<Integer> res = new AVar<>();
-//
-//    AnonymousPipe<Integer> s = pipe.anonymous(Key.wrap("source"));
-//
-//    s.map((i) -> i + 1)
-//     .map(i -> i * 2)
-//     .consume(res::set);
-//
-//    pipe.notify(Key.wrap("source"), 1);
-//
-//    assertThat(res.get(10, TimeUnit.SECONDS), is(4));
-//  }
-//
+
+  @Override
+  protected <T, O> void subscribeAndDispatch(IPipe.PipeEnd<T, O> pipe, List<T> values) {
+    pipe.subscribe(Key.wrap("source", "first"),
+                   firehose);
+
+    for (T v: values) {
+      firehose.notify(Key.wrap("source", "first"), v);
+    }
+  }
+
+  @Override
+  protected <T, O> void subscribeAndDispatch(IPipe.PipeEnd<T, O> pipe, T value) {
+    pipe.subscribe(Key.wrap("source", "first"),
+                   firehose);
+
+    firehose.notify(Key.wrap("source", "first"), value);
+  }
+
 //  @Test
 //  public void testUnregister() throws InterruptedException {
 //    NamedPipe<Integer> pipe = new NamedPipe<>(firehose);
@@ -173,69 +81,5 @@ public class AnonymousPipeTest extends AbstractFirehoseTest {
 //    assertThat(res.get(1, TimeUnit.SECONDS), is(4));
 //  }
 //
-//  @Test
-//  public void testConsume() throws InterruptedException {
-//    NamedPipe<Integer> pipe = new NamedPipe<>(firehose);
-//    AVar<Integer> resValue = new AVar<>();
-//    AVar<Key> resKey = new AVar<>();
-//
-//    AnonymousPipe<Integer> s = pipe.anonymous(Key.wrap("source"));
-//
-//    s.map((i) -> i + 1)
-//     .map(i -> i * 2)
-//     .consume((k, v) -> {
-//       resKey.set(k);
-//       resValue.set(v);
-//     });
-//
-//
-//    pipe.notify(Key.wrap("source"), 1);
-//
-//    assertThat(resKey.get(1, TimeUnit.SECONDS).getPart(0), is("source"));
-//    assertThat(resValue.get(1, TimeUnit.SECONDS), is(4));
-//  }
-//
-//  @Test
-//  public void testSmoke() throws InterruptedException { // Tests backpressure and in-thread dispatches
-//    Firehose<Key> concurrentFirehose = new Firehose<>(new ConcurrentRegistry<>(),
-//                                                      RingBufferWorkProcessor.create(Executors.newFixedThreadPool(4),
-//                                                                                     256),
-//                                                      4,
-//                                                      new Consumer<Throwable>() {
-//                                                        @Override
-//                                                        public void accept(Throwable throwable) {
-//                                                          System.out.printf("Exception caught while dispatching: %s\n",
-//                                                                            throwable.getMessage());
-//                                                          throwable.printStackTrace();
-//                                                        }
-//                                                      });
-//
-//    int iterations = 2000;
-//    CountDownLatch latch = new CountDownLatch(iterations);
-//
-//    NamedPipe<Integer> pipe = new NamedPipe<>(firehose);
-//    pipe.anonymous(Key.wrap("key1"))
-//        .map((i) -> i + 1)
-//        .map(i -> {
-//          try {
-//            Thread.sleep(10);
-//          } catch (InterruptedException e) {
-//            e.printStackTrace();
-//          }
-//          return i * 2;
-//        })
-//        .consume((i_) -> latch.countDown());
-//
-//    for (int i = 0; i < iterations; i++) {
-//      firehose.notify(Key.wrap("key1"), i);
-//      if (i % 500 == 0) {
-//        System.out.println("Processed " + i + " keys");
-//      }
-//    }
-//
-//    latch.await(5, TimeUnit.MINUTES);
-//    assertThat(latch.getCount(), is(0L));
-//
-//    concurrentFirehose.shutdown();
-//  }
+
 }
--- reactor-pipe/src/test/java/reactor/pipe/MatchedPipeTest.java
+++ reactor-pipe/src/test/java/reactor/pipe/MatchedPipeTest.java
@@ -3,150 +3,57 @@ package reactor.pipe;
 import org.junit.Test;
 import reactor.pipe.concurrent.AVar;
 import reactor.pipe.key.Key;
-import reactor.pipe.registry.KeyMissMatcher;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 
-public class MatchedPipeTest extends AbstractFirehoseTest {
+public class MatchedPipeTest extends AbstractPipeTest {
 
   @Test
-  public void mapTest() throws InterruptedException {
-    AVar<String> res = new AVar<>();
+  public void testMatching() throws InterruptedException {
+    AVar<Integer> res1 = new AVar<>(1);
+    AVar<Integer> res2 = new AVar<>(1);
 
-    Pipe.<Integer>build()
-      .map(i -> i + 1)
-      .map(i -> i * 2)
-      .map(Object::toString)
-      .consume(res::set)
-      .subscribe(key -> {
-        return key.getPart(0).equals("source");
-      },
-                 firehose);
+    for (AVar<Integer> avar : new AVar[]{res1, res2}) {
+      Pipe.<Integer>build()
+        .consume(avar::set)
+        .subscribe(key -> key.getPart(0).equals("source"),
+                   firehose);
+    }
+
+    firehose.notify(Key.wrap("source"), 100);
+
+    assertThat(res1.get(1, TimeUnit.SECONDS), is(100));
+    assertThat(res2.get(1, TimeUnit.SECONDS), is(100));
+  }
+
+
+  @Override
+  protected <T, O> void subscribe(IPipe.PipeEnd<T, O> pipe) {
+    pipe.subscribe((k) -> k.getPart(0).equals("source"),
+                   firehose);
+  }
+
+  @Override
+  protected <T, O> void subscribeAndDispatch(IPipe.PipeEnd<T, O> pipe, List<T> values) {
+    pipe.subscribe((k) -> k.getPart(0).equals("source"),
+                   firehose);
+
+    for(T value: values) {
+      firehose.notify(Key.wrap("source", "first"), value);
+    }
+  }
+
+  @Override
+  protected <T, O> void subscribeAndDispatch(IPipe.PipeEnd<T, O> pipe, T value) {
+    pipe.subscribe((k) -> k.getPart(0).equals("source"),
+                   firehose);
 
     firehose.notify(Key.wrap("source", "first"), 1);
-
-    assertThat(res.get(1, TimeUnit.SECONDS), is("4"));
   }
-  //
-  //  @Test
-  //  public void consumeWithSupplierTest() throws InterruptedException {
-  //    NamedPipe<Integer> pipe = new NamedPipe<>(firehose);
-  //    AVar<Integer> res = new AVar<>();
-  //
-  //    pipe.matched(key -> key.getPart(0).equals("source"),
-  //                 Pipe.<Integer>build()
-  //                   .map(i -> i + 1)
-  //                   .map(i -> i * 2)
-  //                   .consume(() -> (k, v) -> res.set(v)));
-  //
-  //    pipe.notify(Key.wrap("source", "first"), 1);
-  //
-  //    assertThat(res.get(1, TimeUnit.SECONDS), is(4));
-  //  }
-  //
-  //  @Test
-  //  public void statefulMapTest() throws InterruptedException {
-  //    AVar<Integer> res = new AVar<>(3);
-  //    NamedPipe<Integer> intPipe = new NamedPipe<>(firehose);
-  //
-  //    intPipe.matched((key) -> key.getPart(0).equals("source"),
-  //                    Pipe.<Integer>build().map((i) -> i + 1)
-  //                                         .map((Atom<Integer> state, Integer i) -> {
-  //                                                       return state.update(old -> old + i);
-  //                                                     },
-  //                                                     0)
-  //                                         .consume(res::set));
-  //
-  //    intPipe.notify(Key.wrap("source", "1"), 1);
-  //    intPipe.notify(Key.wrap("source", "1"), 2);
-  //    intPipe.notify(Key.wrap("source", "1"), 3);
-  //
-  //    assertThat(res.get(LATCH_TIMEOUT, LATCH_TIME_UNIT), is(9));
-  //  }
-  //
-  //  @Test
-  //  public void testFilter() throws InterruptedException {
-  //    NamedPipe<Integer> pipe = new NamedPipe<>(firehose);
-  //    AVar<Integer> res = new AVar<>();
-  //
-  //    pipe.matched(key -> key.getPart(0).equals("source"),
-  //                 Pipe.<Integer>build()
-  //                   .map(i -> i + 1)
-  //                   .filter(i -> i % 2 != 0)
-  //                   .map(i -> i * 2)
-  //                   .consume(res::set));
-  //
-  //
-  //    pipe.notify(Key.wrap("source"), 1);
-  //    pipe.notify(Key.wrap("source"), 2);
-  //
-  //    assertThat(res.get(1, TimeUnit.SECONDS), is(6));
-  //  }
-  //
-  //  @Test
-  //  public void testPartition() throws InterruptedException {
-  //    NamedPipe<Integer> pipe = new NamedPipe<>(firehose);
-  //    AVar<List<Integer>> res = new AVar<>();
-  //
-  //    pipe.matched(key -> key.getPart(0).equals("source"),
-  //                 Pipe.<Integer>build()
-  //                   .partition((i) -> {
-  //                     return i.size() == 5;
-  //                   })
-  //                   .consume(res::set));
-  //
-  //    pipe.notify(Key.wrap("source"), 1);
-  //    pipe.notify(Key.wrap("source"), 2);
-  //    pipe.notify(Key.wrap("source"), 3);
-  //    pipe.notify(Key.wrap("source"), 4);
-  //    pipe.notify(Key.wrap("source"), 5);
-  //    pipe.notify(Key.wrap("source"), 6);
-  //    pipe.notify(Key.wrap("source"), 7);
-  //
-  //    assertThat(res.get(1, TimeUnit.SECONDS), is(TreePVector.from(Arrays.asList(1, 2, 3, 4, 5))));
-  //  }
-  //
-  //  @Test
-  //  public void testSlide() throws InterruptedException {
-  //    NamedPipe<Integer> pipe = new NamedPipe<>(firehose);
-  //    AVar<List<Integer>> res = new AVar<>(6);
-  //
-  //    pipe.matched((key) -> key.getPart(0).equals("source"),
-  //                 Pipe.<Integer>build()
-  //                   .slide(i -> i.subList(i.size() > 5 ? i.size() - 5 : 0,
-  //                                         i.size()))
-  //                   .consume(res::set));
-  //
-  //    pipe.notify(Key.wrap("source"), 1);
-  //    pipe.notify(Key.wrap("source"), 2);
-  //    pipe.notify(Key.wrap("source"), 3);
-  //    pipe.notify(Key.wrap("source"), 4);
-  //    pipe.notify(Key.wrap("source"), 5);
-  //    pipe.notify(Key.wrap("source"), 6);
-  //
-  //    assertThat(res.get(1, TimeUnit.SECONDS), is(TreePVector.from(Arrays.asList(2, 3, 4, 5, 6))));
-  //  }
-  //
-  //  @Test
-  //  public void testMatching() throws InterruptedException {
-  //    NamedPipe<Integer> pipe = new NamedPipe<>(firehose);
-  //    AVar<Integer> res1 = new AVar<>(1);
-  //    AVar<Integer> res2 = new AVar<>(1);
-  //
-  //    for (AVar<Integer> avar : new AVar[]{res1, res2}) {
-  //      pipe.matched(key -> key.getPart(0).equals("source"),
-  //                   Pipe.<Integer>build()
-  //                     .consume(avar::set));
-  //    }
-  //
-  //    firehose.notify(Key.wrap("source"), 100);
-  //
-  //    assertThat(res1.get(1, TimeUnit.SECONDS), is(100));
-  //    assertThat(res2.get(1, TimeUnit.SECONDS), is(100));
-  //  }
 
 
 }
