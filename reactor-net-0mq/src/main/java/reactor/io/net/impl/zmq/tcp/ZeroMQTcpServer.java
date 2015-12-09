1910714ee0ed0a9827b3ee1b2dc63616f86c229c
Stephane Maldini <smaldini@pivotal.io>
2015-12-09 23:46:37 +0000

    fix build issues

--- reactor-net-0mq/src/main/java/reactor/io/net/impl/zmq/tcp/ZeroMQTcpServer.java
+++ reactor-net-0mq/src/main/java/reactor/io/net/impl/zmq/tcp/ZeroMQTcpServer.java
@@ -29,6 +29,7 @@ import org.zeromq.ZContext;
 import org.zeromq.ZFrame;
 import org.zeromq.ZMQ;
 import org.zeromq.ZMsg;
+import reactor.core.subscriber.BaseSubscriber;
 import reactor.core.support.Assert;
 import reactor.core.support.NamedDaemonThreadFactory;
 import reactor.core.support.UUIDUtils;
@@ -45,7 +46,6 @@ import reactor.io.net.tcp.TcpServer;
 import reactor.rx.Promise;
 import reactor.rx.Promises;
 import reactor.rx.Stream;
-import reactor.rx.action.support.DefaultSubscriber;
 import reactor.rx.broadcast.Broadcaster;
 import reactor.rx.broadcast.SerializedBroadcaster;
 import reactor.rx.stream.GroupedStream;
@@ -148,7 +148,7 @@ public class ZeroMQTcpServer extends TcpServer<Buffer, Buffer> {
 											.setConnectionId(stringZMsgGroupedStream.key())
 											.setSocket(socket);
 
-							handler.apply(netChannel).subscribe(new DefaultSubscriber<Void>(){
+							handler.apply(netChannel).subscribe(new BaseSubscriber<Void>(){
 								@Override
 								public void onSubscribe(Subscription s) {
 									s.request(Long.MAX_VALUE);
